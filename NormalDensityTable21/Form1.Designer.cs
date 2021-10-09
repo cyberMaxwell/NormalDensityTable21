30982967d97f2ed69d105e5aa68fa07255cc3ebd
user <user@HP>
2021-10-09 22:48:39 +0200

    Добавьте файлы проекта.

---
+++ NormalDensityTable21/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace NormalDensityTable21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.MarkerSize = 10;
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(530, 426);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.OnPlotClick);
+            this.chart1.Paint += new System.Windows.Forms.PaintEventHandler(this.plot);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(548, 118);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(240, 320);
+            this.dataGridView.TabIndex = 2;
+            this.dataGridView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.onCellValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.on);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ NormalDensityTable21/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace NormalDensityTable21
+{
+
+    public partial class Form1 : Form
+    {
+
+        public static double f(double x)
+        {
+            return x * x * x - 3 * x * x - x + 3;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView.Columns.Add("X", "x");
+            dataGridView.Columns.Add("Y", "f(x)");
+
+        }
+
+
+        private void onCellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+
+            double x, y;
+            bool isDouble = double.TryParse(dataGridView.Rows[e.RowIndex].Cells[0].Value.ToString(), out _);
+            if (!isDouble)
+            {
+
+                dataGridView.Rows[e.RowIndex].Cells[0].Value = DBNull.Value;
+            }
+            else
+            {
+                dataGridView.Rows[e.RowIndex].Cells[1].Value = f(Convert.ToDouble(dataGridView.Rows[e.RowIndex].Cells[0].Value));
+
+                x = Convert.ToDouble(dataGridView.Rows[e.RowIndex].Cells[1].Value);
+                y = f(x);
+
+
+                
+                chart1.Series[1].MarkerStyle = MarkerStyle.Circle;
+                chart1.Series[1].Color = Color.Red;
+                chart1.Series[1].Points.Add(new DataPoint(x, y));
+             
+            }
+
+        }
+
+        private void OnPlotClick(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void on(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plot(object sender, PaintEventArgs e)
+        {
+
+
+            double a = -10, b = 10, h = 0.1, x, y;
+
+            chart1.Series[0].Points.Clear();
+
+
+            chart1.ChartAreas[0].AxisX.Minimum = -3;
+            chart1.ChartAreas[0].AxisX.Maximum = 4;
+            chart1.ChartAreas[0].AxisY.Minimum = -10;
+            chart1.ChartAreas[0].AxisY.Maximum = 10;
+
+
+
+            x = a;
+            while (x <= b)
+            {
+
+                chart1.Series[0].Points.AddXY(x, f(x));
+
+
+                x += h;
+            }
+
+        }
+
+        private void tick(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
